0aed43f50d7ecee1db6bb9264b7e1b3a9bf7d9bb
Saki <ksaki0402@gmail.com>
2026-04-22 14:07:37 +0900

    Initial Commit

---
+++ Kireicchi/KireicchiApp.swift
@@ -0,0 +1,17 @@
+//
+//  KireicchiApp.swift
+//  Kireicchi
+//
+//  Created by 神林沙希 on 2026/04/22.
+//
+
+import SwiftUI
+
+@main
+struct KireicchiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
